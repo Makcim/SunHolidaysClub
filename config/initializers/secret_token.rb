dc83d9ac816b132970498b83cded1ad591dd245b
Max <Makcim@ukrpost.net>
2011-11-17 01:12:07 +0200

    initial uploading

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shc::Application.config.secret_token = '402e461e9b596932221389a0514f0da7a1584903374c2a68cfeec21dd9b088baa75c8763170c3b44fa0b38554008331019e10113f8dcba1e4b8aaabe07c4a779'
